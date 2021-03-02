416c5f06b3f27d92b229bfb8a0cde0d2d21b3be8
javy <javyho@163.com>
2021-03-02 16:59:30 +0800

    login and list

---
+++ data/cars.js
@@ -0,0 +1,33 @@
+var cars = [{
+  "name": "别克",
+  "des": "7座商务车",
+  "carNum":"粤A00001",
+  "url": "../../image/car/GL8.jpg",
+  "startTime": "09:00",
+  "endTime": "18:00"
+}, {
+  "name": "别克",
+  "des": "7座商务车",
+  "carNum":"粤A00002",
+  "url": "../../image/car/GL8.jpg",
+  "startTime": "09:00",
+  "endTime": "12:00"
+}, {
+  "name": "别克",
+  "des": "7座商务车",
+  "carNum":"粤A00003",
+  "url": "../../image/car/GL8.jpg",
+  "startTime": "14:00",
+  "endTime": "18:00"
+}, {
+  "name": "别克",
+  "des": "7座商务车",
+  "carNum":"粤A00004",
+  "url": "../../image/car/GL8.jpg",
+  "startTime": "10:00",
+  "endTime": "15:00"
+}]
+
+module.exports = {
+  carJsonList:cars
+}
